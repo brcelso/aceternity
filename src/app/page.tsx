9d230795f9a43c55b9bd3b05afd1bbef1571f6c3
celso <celso.sjunior@outlook.com>
2024-04-08 04:09:56 +0000

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <h1 className="text-2x1 text-center">Chai aur code </h1>
-      <HeroSection />
+      
     </main>
   );
 }
