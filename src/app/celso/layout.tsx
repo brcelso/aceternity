158860927f3b08d0844b6e8cc4d58dbf3395eb9f
celso <celso.sjunior@outlook.com>
2024-04-08 01:40:34 +0000

    ok

---
+++ src/app/celso/layout.tsx
@@ -0,0 +1,17 @@
+
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+        <h2>inner layout item</h2>
+        {children}
+    </>
+            
+  );
+}
---
+++ src/app/celso/page.tsx
@@ -0,0 +1,6 @@
+export default function celsoPage(){
+    return(
+        <h1 className="text-3x1 text-center
+        mt-8">Celso Page</h1>
+    )
+}
