7812b4b10ecfa1e00040d0fb6c30ee48a5f2933d
celso <celso.sjunior@outlook.com>
2024-04-08 02:38:52 +0000

    dark ok

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,11 +13,18 @@ function NavBar({ className }: { className?: string }) {
         className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
         >
             <Menu setActive={setActive}>
-                <Link href={"#"}>
+                <Link href={"/"}>
                 <MenuItem setActive={setActive} active={active} item="Home">
-                test
+                
                 </MenuItem>
                 </Link>
+                <MenuItem setActive={setActive} active={active} item="Our Courses">
+                    <div className="flex flex-col space-y-4 text-sm">
+                    <HoveredLink href='/courses'>All courses
+                    </HoveredLink>
+                    </div>
+                    
+                </MenuItem>
             </Menu>
         </div>
     )
