52c0e5138a5f77ed0f530f435a50f2e3f4bfe5f0
celso <celso.sjunior@outlook.com>
2024-04-08 04:08:48 +0000

    ok

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,8 +8,7 @@ function HeroSection() {
         <div className="p-4 relative z-10 w-full text-center"
         >
             <h1
-            className="mt-20 md:mt-0 text-4xl md:text-7xl
-            font-bold bg-clip-text "
+            
             >Master the art of music</h1>
             <p
             
