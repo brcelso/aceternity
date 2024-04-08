9c01f94fa4cef24cff9689789b9ec0b72f794f8c
celso <celso.sjunior@outlook.com>
2024-04-08 04:06:35 +0000

    ok

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -14,8 +14,7 @@ function HeroSection() {
             font-bold bg-clip-text "
             >Master the art of music</h1>
             <p
-            className="mt-4 font-normal text-base md:text-lg
-            text-neutral-300 max-w-lg mx-auto"
+            
             >Dive into our comprehensive music courses and
             transform your musical journey today. Whether
             you're a beginner or looking to refine your 
